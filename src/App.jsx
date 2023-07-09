5fecaa0fdeb4c59ab5c14f87937d184663c2010a
Carlos <ordonez456.1998@gmail.com>
2023-07-09 01:22:23 -0500

    Creacion del index

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
+import React from 'react';
+import './App.css'; // Archivo CSS con los estilos adicionales
+import { styled } from '@mui/material/styles'; // Import correcto para makeStyles
+import Typography from '@mui/material/Typography'; // Import correcto para Typography
 
-import './App.css'
+const Container = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  minHeight: '100vh',
+});
+
+const Title = styled(Typography)({
+  marginBottom: '16px',
+});
+
+const Subtitle = styled(Typography)({
+  opacity: 0.5,
+  marginBottom: '16px',
+});
 
 function App() {
 
   return (
-    <>
-      <p>
+    <Container>
+      <Title variant="h4">
         Calculadora
-      </p>
-    </>
+      </Title>
+      <Subtitle variant="body1">
+        Calculadora creada con Material-UI
+      </Subtitle>
+      {/* Espacio para la calculadora */}
+    </Container>
   )
 }
 
